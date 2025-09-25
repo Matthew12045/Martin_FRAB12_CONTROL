31e5133cb0d7de5b8460bb522eb10d7c7b4e0d46
Matthew12045 <brush.zeglarz@gmail.com>
2025-09-26 01:25:15 +0700

    Revert "keyboard node named eiei and launchfile"
    This reverts commit d94b015c35fc7a098c2f09ee1d8beb9190be84fd.

--- ros2_ws/install/setup.ps1
+++ ros2_ws/install/setup.ps1
@@ -23,7 +23,6 @@ function _colcon_prefix_chain_powershell_source_script {
 
 # source chained prefixes
 _colcon_prefix_chain_powershell_source_script "/opt/ros/humble\local_setup.ps1"
-_colcon_prefix_chain_powershell_source_script "/home/martin/FIBOX/Martin_FRAB12_CONTROL/LOW_LEVEL/install\local_setup.ps1"
 
 # source this prefix
 $env:COLCON_CURRENT_PREFIX=(Split-Path $PSCommandPath -Parent)
