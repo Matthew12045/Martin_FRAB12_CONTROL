aca7aaf75d6403b6dd4e3d2d8a0a965d59c5df58
Matthew12045 <93376256+Matthew12045@users.noreply.github.com>
2025-09-26 02:11:48 +0700

    Delete ros2_ws/build directory
    del

--- ros2_ws/build/marble_robot_control/CMakeFiles/marble_robot_control_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/marble_robot_control_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/marble_robot_control_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
